009a681045295088bbf90b6a0085ff0156ba8887
emmazhuang <zzmmdd610@gmail.com>
2017-07-10 02:27:09 -0700

    es+django+redis

---
+++ onem3_spider/tools/test.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+__author__ = 'emma'
+
+#写一个test先测试一下一小段代码
+
+import redis
+redis_cli = redis.StrictRedis()
+redis_cli.incr("onem3point_count")
+
+"""
+Emmas-MacBook-Pro:redis-3.2.9 emmazhuang$ src/redis-cli 
+#运行一次后
+127.0.0.1:6379> keys *
+1) "onem3point_count"
+127.0.0.1:6379> type onem3point_count
+string
+127.0.0.1:6379> get onem3point_count
+"1"
+#再运行一次后
+127.0.0.1:6379> get onem3point_count
+"2"
+127.0.0.1:6379> 
+"""
